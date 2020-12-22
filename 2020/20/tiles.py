db6f1b969f2b5e8df5b9162ac025a530915b5e6c
Sven Westergren <sven.westergren@intel.com>
2020-12-22 21:18:39 +0100

    Cleanup Day 20

--- 2020/20/tiles.py
+++ 2020/20/tiles.py
@@ -7,20 +7,22 @@ sample_grid = [[1951, 2311, 3079],
                [2971, 1489, 1171]]
 sample_sum = 20899048083289
 
-WEST  = (-1,  0)
-EAST  = ( 1,  0)
-NORTH = ( 0, -1)
-SOUTH = ( 0,  1)
-
 sea_monster = [r"                  # ",
                r"#    ##    ##    ###",
                r" #  #  #  #  #  #   "]
 sea_rex = [x.replace(' ', '.') for x in sea_monster]
 hashes_in_monster = "".join(sea_monster).count('#')
 
+WEST = 1
+EAST = -WEST
+NORTH = 2
+SOUTH = -NORTH
+
 class Tile:
     def __init__(self, data = None, num = None, tile = None,
                  rotation = 0, flip = 0):
+        """Initialize with raw data or num and tile. Rotation and flip is just
+           for representation"""
         if num and tile:
             self.num = num
             self.tile = tile
@@ -28,13 +30,13 @@ class Tile:
             title, tile = data.split(':')
             self.num = int(title.split()[1])
             self.tile = tuple([tuple(x) for x in tile.strip().split('\n')])
-        self.rotation = rotation
-        self.flip = flip
-        self.exits = {}
         self.edges = {WEST:  tuple((r[0] for r in self.tile)),
                       EAST:  tuple((r[-1] for r in self.tile)),
                       NORTH: self.tile[0],
                       SOUTH: self.tile[-1]}
+        self.exits = {}
+        self.rotation = rotation
+        self.flip = flip
 
     def __repr__(self):
         return f"<Tile {self.num}:{self.flip}.{self.rotation}>"
@@ -56,53 +58,39 @@ class Tile:
         return r
 
     def aligns(self, other, direction):
-        """Returns true if this tile aligns with other in 'direction'"""
+        """Returns True if edge in 'direction' aligns with other"""
         if self.num == other.num:
             return False
-        reverse = (-direction[0], -direction[1])
-        return self.edges[direction] == other.edges[reverse]
+        return self.edges[direction] == other.edges[-direction]
 
     def set_exits(self, edict):
-        self.exits = {}
+        """Find the matching tile, if any, for all edges"""
         for d in self.edges:
-            r = {t for t in edict[self.edges[d]]
-                 if self.aligns(t, d)}
-            assert(len(r) in [0, 1])
+            r = {t for t in edict[self.edges[d]] if self.aligns(t, d)}
             self.exits[d] = r.pop() if r else None
 
     def rotated(self, n = 1):
+        """Return this tile rotated n steps"""
         for _ in range(n):
-            tile = tuple(zip(*self.tile[::-1]))
+            tile = tuple(rotate(self.tile))
         return Tile(num = self.num, tile = tile,
                     rotation = self.rotation + n, flip = self.flip)
 
     def flipped(self):
+        """Return this tile flipped horizontally"""
         tile = list(reversed(self.tile))
         return Tile(num = self.num, tile = tile,
                     rotation = self.rotation, flip = 0 if self.flip else 1)
 
-def corner_key(t):
-    r = 's' if t.exits[SOUTH] is None else 'n'
-    r += 'w' if t.exits[WEST] is None else 'e'
-    return r
+def rotate(grid):
+    return zip(*grid[::-1])
 
 def parse(data):
     data = data.split('\n\n')
     tiles = [Tile(d) for d in data]
-    edict = {}
-    for t in tiles:
-        for e in t.all_variant_edges():
-            edict.setdefault(e, set()).add(t)
 
-    corners = [t for t in tiles
-               if len([e for e in t.edges.values()
-                       if len(edict[e]) == 1]) == 2]
-    part1 = 1
-    for c in corners:
-        part1 *= c.num
-
-    # add all variant tiles
-    variants = set(tiles)
+    # Create all variant tiles
+    variants = set()
     for t in tiles:
         for _ in range(2):
             t = t.flipped()
@@ -110,20 +98,30 @@ def parse(data):
                 t = t.rotated()
                 variants.add(t)
 
-    # create an edge-dictionary of all variant edges
+    # Create an edge-dictionary of all variant edges
     vdict = {}
     for t in variants:
         for e in t.edges.values():
             vdict.setdefault(e, set()).add(t)
-    # set the exits of all variants
+    # Set the exits of all variant tiles
     for t in variants:
         t.set_exits(vdict)
 
+    # Find the corners
+    corners = {v.num for v in variants if
+               sum([bool(e) for e in v.exits.values()]) == 2}
+
+    part1 = 1
+    for c in corners:
+        part1 *= c
+
+    # Find all variant NW corners
     nwc = [v for v in variants
-           if v.num in [c.num for c in corners]
-           and corner_key(v) == 'nw']
-    nwc.sort(key = lambda x: repr(x))
-    # pick one variant and trace it
+           if v.num in corners
+           and v.exits[NORTH] is None
+           and v.exits[WEST] is None]
+
+    # Pick one NW corner and trace it to build a grid
     tile = nwc[0]
     dim = int(math.sqrt(len(tiles)))
     grid = [[None] * dim for _ in range(dim)]
@@ -136,6 +134,8 @@ def parse(data):
                 grid[y][x] = grid[y-1][x].exits[SOUTH]
             else:
                 grid[y][x] = grid[y][x-1].exits[EAST]
+
+    # Collect all lines to build the image
     image = []
     for row in grid:
         for i in range(1, 9):
@@ -144,16 +144,20 @@ def parse(data):
                 rr += t.get_line(i)
             image.append(rr)
 
+    # Rotate and flip the image while looking for monsters
     img = list(map(tuple, image))
     mc = (0, None)
     for _ in range(2):
         img = list(reversed(img))
         for _ in range(4):
-            img = ["".join(x) for x in (zip(*img[::-1]))]
+            img = ["".join(x) for x in rotate(img)]
             c = find_monsters(img)
             if c > mc[0]:
                 mc = (c, img)
+
+    # Count the number of '#' and subtract all monster-'#'
     part2 = "".join(mc[1]).count('#') - mc[0] * hashes_in_monster
+
     return part1, part2
 
 def find_monsters(image):
@@ -168,8 +172,10 @@ def find_monsters(image):
 def test():
     with open('sample') as f:
         data = f.read()
-        print("test", parse(data))
-    return True
+    part1, part2 = parse(data)
+    print("test1", part1)
+    print("test2", part2)
+    return part1 == 20899048083289 and part2 == 273
 
 if __name__ == "__main__":
     import sys
@@ -180,4 +186,4 @@ if __name__ == "__main__":
             data = f.read().strip()
             part1, part2 = parse(data)
             print("part1", part1)
-            print("part1", part2)
+            print("part2", part2)
